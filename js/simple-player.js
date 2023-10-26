5ab1b54c9027f1d4d0163c9d0be42665f136a2e5
Imankoray <146858173+Imankoray@users.noreply.github.com>
2023-10-26 15:53:57 +0100

    audio

--- js/simple-player.js
+++ js/simple-player.js
@@ -1,3 +1,75 @@
+
+
+// use DOM to get a refetence to the UI elements 
+const button= document.getElementById("play-pause-button");
+const currentTime =  document.getElementById( "current-time");
+const totalTime = document.getElementById("total-time");
+const seekbar = document.getElementById("seek-bar");
+const audio = new Audio("audio/Soft-Background-for-Interview.webm");
+
+// seek state - are we seeking?
+let seeking = false
+
+//listen to button click using arrow function
+button.onclick = (event) =>{
+if(audio.paused == true){
+    audio.play();
+}else{
+    audio.pause();
+}
+}
+
+// audio object listeners
+audio.onplay = (event) =>{
+    button.src = "images/pause.svg"
+}
+audio.onpause = (event) =>{
+    button.src = "images/play.svg"
+}
+
+audio.onloadedmetadata = (event) =>{
+    currentTime.innerHTML = formatTime(0);
+    totalTime.innerHTML = formatTime(audio.duration);
+    seekbar.max = Math.floor(audio.duration);
+    seekbar.value = 0;
+}
+
+audio.oncanplaythrough =(event) =>{
+    seekbar.disabled = false;
+}
+
+audio.ontimeupdate = (event) =>{
+    currentTime.innerHTML = formatTime(audio.currentTime)
+
+// seekbar listeners 
+seekbar.oninput = (event) =>{
+    seeking=true;
+}
+
+// curly brackets is the event you want to happen
+seekbar.onchange = (event) =>{
+    audio.currentTime= seekbar.value;
+    if(!audio.paused){
+        audio.play();
+    }
+    seeking= false
+}
+    
+    // ! means false/not. if seeking = false 
+    if(!seeking){
+        seekbar.value = Math.floor(audio.currentTime);
+
+
+}
+
+}
+
+
+
+
+
+
+// helper functions
 function formatTime(secs) {
     let hours = Math.floor(secs / 3600);
     let minutes = Math.floor((secs - (hours * 3600)) / 60);
